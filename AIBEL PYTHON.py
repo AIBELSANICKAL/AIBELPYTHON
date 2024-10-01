24371dca6115d0f4105e976b6b38f69c70613f3c
AIBELSANICKAL <aibelsanickal2028@cs.sjcetpalai.ac.in>
2024-10-01 14:54:37 +0530

    Add files via upload
    my first python coding

---
+++ AIBEL PYTHON.py
@@ -0,0 +1,7 @@
+print("hello python")
+name=input("enter your name:")
+print(name)
+roll_no=input("enter the roll number:")
+print("name of the student:",name)
+cgpa=float(input("enter your cgpa:"))
+percentagecgpa=10
