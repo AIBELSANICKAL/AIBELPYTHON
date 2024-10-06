21a5327610ddf7363197e12ad321795fccfe827c
AIBELSANICKAL <aibelsanickal2028@cs.sjcetpalai.ac.in>
2024-10-06 21:48:58 +0530

    Update aibel python task 2.py

--- aibel python task 2.py
+++ aibel python task 2.py
@@ -4,7 +4,8 @@ date: 05-10-2024
 version:1.0''
 '''
 str1="Hello World!"
-str2=str1[:5]
-str3="Everyone"
-print(str2 , str3)
+str2=str1[6:11]
+print(str2)
+str3=str2 +"everyone!"
+print("new output is: ",str3)
 
