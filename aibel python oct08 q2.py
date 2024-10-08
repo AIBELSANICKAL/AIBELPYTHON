4e9a7b02900d30f3b7957898885a4938bd382878
AIBELSANICKAL <aibelsanickal2028@cs.sjcetpalai.ac.in>
2024-10-08 14:59:43 +0530

    Add files via upload
    Title: Create, concatenate, and print a string and access a sub-string from a given string.
    
    Write a Python program that performs the following tasks:

---
+++ aibel python oct08 q2.py
@@ -0,0 +1,10 @@
+''''''
+NAME: AIBEL S ANICKAL
+DATE:08-10-2024
+VERSION:1.0''''''
+
+first_name=input("enter your name:")
+last_name=input("enter your last name:")
+full_name=first_name
+print(full_name)
+
