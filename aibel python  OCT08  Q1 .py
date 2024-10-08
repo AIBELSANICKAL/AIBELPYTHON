8a49769d97e9f18a1b59f5bc8ff1302385371585
AIBELSANICKAL <aibelsanickal2028@cs.sjcetpalai.ac.in>
2024-10-08 14:40:55 +0530

    Add files via upload
    Title: Simple desktop calculator using Python. Only the five basic arithmetic operators.
    
    AIM: Write a Python program that performs the following tasks

---
+++ aibel python  OCT08  Q1 .py
@@ -0,0 +1,22 @@
+num1=(int(input("enter the number:")))
+num2=(int(input("enter the number:")))
+num3=(int(input("enter the number")))
+
+#addition
+sum=num1+num2
+print("the sum of num1 and num2:",sum)
+#subraction
+sub=num2-num1
+print("the difference when num2 is subracted from num1:",sub)
+#multiplication
+multiplication=num1*num2
+print("the product of num1 and num2:",multiplication)
+#division
+divide=num1/num2
+print("the quotient when num1 is divided by num2:",divide)
+#modulus
+mod=num1%num2
+print("the remainder when num1 is divided by num2:",mod)
+#combined arithemetic operations
+combined=(num1+num2)*num3/num2
+print("the result of num1+num2*num3/num2 is:",combined)
