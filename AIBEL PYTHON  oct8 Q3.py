30b080cddd9fb7394052089c852e4defdb12c9d2
AIBELSANICKAL <aibelsanickal2028@cs.sjcetpalai.ac.in>
2024-10-08 16:13:33 +0530

    Add files via upload
    Title: Familiarize time and date in various formats (Eg. “Thu Jul 11 10:26:23 IST 2024”).

---
+++ AIBEL PYTHON  oct8 Q3.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+print(datetime.now())
+current_time=datetime.now()
+format1=current_time.strftime("%Y-%m-%d %h:%m:%s")
+print(format1)
+format2=current_time.strftime("%d-%B-%y ")
+print(format2)
