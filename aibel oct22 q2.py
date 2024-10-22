7e3e718be268ea1dec306a6fa253b2f55f8d6307
AIBELSANICKAL <aibelsanickal2028@cs.sjcetpalai.ac.in>
2024-10-22 14:29:47 +0530

    Update aibel oct22 q2.py

--- aibel oct22 q2.py
+++ aibel oct22 q2.py
@@ -1,8 +1,8 @@
 tempurature=float(input("enter the tempurature:"))
 unit=input("is the tempurature is fahrenheit or celsius(c or f:")
-if unit=="c" or unit=="c":
+if unit=="c" or unit=="C":
     faran=(9/5*tempurature)+32
     print(faran,"f")
-elif unit=="f" or unit=="f":
+elif unit=="f" or unit=="F":
     cels=5/9*(tempurature-32)
     print(cels,"c")
