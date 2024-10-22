97ee8a91d7e4f442e2086b721bd4b53516f32784
AIBELSANICKAL <aibelsanickal2028@cs.sjcetpalai.ac.in>
2024-10-22 14:24:46 +0530

    Add files via upload

---
+++ aibel oct22 q2.py
@@ -0,0 +1,8 @@
+tempurature=float(input("enter the tempurature:"))
+unit=input("is the tempurature is fahrenheit or celsius(c or f:")
+if unit=="c" or unit=="c":
+    faran=(9/5*tempurature)+32
+    print(faran,"f")
+elif unit=="f" or unit=="f":
+    cels=5/9*(tempurature-32)
+    print(cels,"c")
