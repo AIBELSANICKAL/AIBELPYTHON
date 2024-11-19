07ecb0a0541a7b14b3f3db50eee5759f2b495618
AIBELSANICKAL <aibelsanickal2028@cs.sjcetpalai.ac.in>
2024-11-19 10:50:32 +0530

    Add files via upload

---
+++ aibel python nov 19.py
@@ -0,0 +1,20 @@
+players=['messi','ronaldo','lewendoski','hamilltton']
+promotions=['adidas','nike','puma','hublot']
+print(players[1])
+print(players[2])
+print(players[3])
+combined_tuple=players+promotions
+print("the combined tuple",combined_tuple)
+repeat_tuple=("millions",)*3
+print(repeat_tuple)
+
+
+
+colors=('violet','indigo','blue','orange')
+print(colors[-2])
+person=("aibel",18,"student")
+name,age,job=person
+print(name)
+print(age)
+print(job)
+
